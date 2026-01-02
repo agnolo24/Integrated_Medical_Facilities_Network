716edb6fa9a846f841c0223ad62dfbc97a5481cd
agnolo24 <agnolo2024@gmail.com>
2026-01-02 15:35:37 +0530

    hospital registration form created

--- frontend/imfn/src/App.js
+++ frontend/imfn/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import RegForm1 from './components/userRegForm/RegForm1'
-
+import HospitalRegForm from './components/hospitalRegForm/HospitalRegForm'
 function App() {
   return (
     <div>
-        <RegForm1/>
+          <RegForm1 />
+          <HospitalRegForm />  
     </div>
   )
 }
