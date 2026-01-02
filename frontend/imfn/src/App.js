d83936ecf640bc635ce6dcfbca00b2f8e1fad43b
Amaljith <agnolo2024@gmail.com>
2026-01-02 14:40:41 +0530

    created the frontend and add a registration form

---
+++ frontend/imfn/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegForm1 from './components/userRegForm/RegForm1'
+
+function App() {
+  return (
+    <div>
+        <RegForm1/>
+    </div>
+  )
+}
+
+export default App
