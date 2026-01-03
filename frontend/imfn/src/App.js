aec851c3047aebe77c13735c998c29156adfadaf
Diane <dianemathew2002@gmail.com>
2026-01-03 11:19:09 +0530

    created an ambulance regForm

--- frontend/imfn/src/App.js
+++ frontend/imfn/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import RegForm1 from './components/forms/userRegForm/RegForm1'
 import HospitalRegForm from './components/forms/hospitalRegForm/HospitalRegForm'
+import AmbulanceForm from './components/forms/AmbulanceForm/AmbulanceForm'
+
 function App() {
   return (
     <div>
           <RegForm1 />
-          <HospitalRegForm />  
+          <HospitalRegForm /> 
+          <AmbulanceForm/>
     </div>
   )
 }
