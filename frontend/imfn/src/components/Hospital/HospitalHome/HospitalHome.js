959472c2bb9e5e6c9d56da90a35812e8b1fbf82f
agnolo24 <agnolo2024@gmail.com>
2026-01-11 18:32:14 +0530

    added header and footer to hospital home page

--- frontend/imfn/src/components/Hospital/HospitalHome/HospitalHome.js
+++ frontend/imfn/src/components/Hospital/HospitalHome/HospitalHome.js
@@ -1,11 +1,15 @@
 import React from "react";
+import HospitalHeader from "../HospitalHeader/HospitalHeader";
+import HospitalFooter from "../HospitalFooter/HospitalFooter";
 
 function HospitalHome(){
 
     return(
 
         <div>
-            Hospital Home Page
+            <HospitalHeader/>
+            <h1><center>Hospital Home Page</center></h1>
+            <HospitalFooter/>
         </div>
     )
 }
