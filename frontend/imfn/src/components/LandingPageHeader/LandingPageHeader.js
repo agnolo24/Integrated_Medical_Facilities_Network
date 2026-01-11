6b5e28c86da9099af0dc3542ee23b39a1da8fffe
agnolo24 <agnolo2024@gmail.com>
2026-01-11 18:19:02 +0530

    made the LandingPageHeader.js optimized for all screen size

--- frontend/imfn/src/components/LandingPageHeader/LandingPageHeader.js
+++ frontend/imfn/src/components/LandingPageHeader/LandingPageHeader.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router';
 
 // CSS Imports
@@ -14,6 +14,28 @@ import '../../asset/user_assets/css/style.css';
 import '../../asset/user_assets/css/responsive.css';
 
 function LandingPageHeader() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 150) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div>
             {/* header */}
@@ -44,25 +66,23 @@ function LandingPageHeader() {
                                         <a href="#" title="Twitter"><i className="fab fa-twitter"></i></a>
                                         <a href="#" title="LinkedIn"><i className="fab fa-linkedin-in"></i></a>
                                     </span>
-                                    {/*  /social media icon redux */}
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
-                <div id="header-sticky" className="menu-area">
+                <div id="header-sticky" className={`menu-area ${isSticky ? 'sticky-menu' : ''}`}>
                     <div className="container">
                         <div className="second-menu">
                             <div className="row align-items-center">
-                                <div className="col-xl-2 col-lg-2">
-                                    <div className="logo">
+                                <div className="col-xl-2 col-lg-2 col-md-6 col-6">
+                                    <div className="logo" style={{ background: 'none', width: '100%' }}>
                                         <a href="index.html"><img src={require('../../asset/user_assets/img/logo/logo.png')} alt="logo" /></a>
                                     </div>
                                 </div>
-                                <div className="col-xl-8 col-lg-8">
+                                <div className="col-xl-8 col-lg-8 d-none d-lg-block">
                                     <div className="main-menu text-right pr-15">
-                                        <nav id="mobile-menu">
+                                        <nav id="mobile-menu-desktop">
                                             <ul>
                                                 <li className="has-sub">
                                                     <NavLink to="/"><h6>Home</h6></NavLink>
@@ -97,20 +117,66 @@ function LandingPageHeader() {
                                 </div>
                                 <div className="col-xl-2 col-lg-2 d-none d-lg-block">
                                     <NavLink to="/login"><button className="top-btn">Login</button></NavLink>
-
                                 </div>
-                                <div className="col-12">
-                                    <div className="mobile-menu"></div>
+                                <div className="col-md-6 col-6 d-block d-lg-none">
+                                    <div className="mobile-menu-toggle d-flex justify-content-end align-items-center">
+                                        <div className="responsive" onClick={toggleMobileMenu} style={{ display: 'block', cursor: 'pointer', marginTop: 0 }}>
+                                            <i className="fas fa-bars" style={{ fontSize: '25px', color: '#1E0B9B' }}></i>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </header>
-            {/* header-end */}
+            {/* Header End */}
 
+            {/* Offcanvas Menu */}
+            <div className={`offcanvas-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+                <span className="menu-close" onClick={toggleMobileMenu}>
+                    <i className="fas fa-times"></i>
+                </span>
+
+                <ul className="d-block" style={{ marginTop: '40px' }}>
+                    <li><NavLink to="/" onClick={toggleMobileMenu}>Home</NavLink></li>
+                    <li><a href="about.html" onClick={toggleMobileMenu}>About Us</a></li>
+                    <li>
+                        <a href="#" onClick={(e) => e.preventDefault()}>Services</a>
+                        <ul className="sub-menu pl-3" style={{ display: 'block' }}>
+                            <li><a href="services.html" onClick={toggleMobileMenu}>Services</a></li>
+                            <li><a href="services-detail.html" onClick={toggleMobileMenu}>Services Details</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" onClick={(e) => e.preventDefault()}>Blog</a>
+                        <ul className="sub-menu pl-3" style={{ display: 'block' }}>
+                            <li><a href="blog.html" onClick={toggleMobileMenu}>Blog</a></li>
+                            <li><a href="blog-details.html" onClick={toggleMobileMenu}>Blog Details</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" onClick={(e) => e.preventDefault()}>Registration</a>
+                        <ul className="sub-menu pl-3" style={{ display: 'block' }}>
+                            <li><NavLink to="/userReg" onClick={toggleMobileMenu}>Patient Registration</NavLink></li>
+                            <li><NavLink to="/hospitalReg" onClick={toggleMobileMenu}>Hospital Registration</NavLink></li>
+                            <li><NavLink to="/ambulance" onClick={toggleMobileMenu}>Ambulance Registration</NavLink></li>
+                        </ul>
+                    </li>
+                    <li><NavLink to="/login" onClick={toggleMobileMenu}>Login</NavLink></li>
+                </ul>
+
+                <div className="side-social" style={{ marginTop: '20px' }}>
+                    <a href="#"><i className="fab fa-facebook-f"></i></a>
+                    <a href="#"><i className="fab fa-twitter"></i></a>
+                    <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i className="fab fa-google-plus-g"></i></a>
+                </div>
+            </div>
+
+            <div className={`offcanvas-overly ${isMobileMenuOpen ? 'active' : ''}`} onClick={toggleMobileMenu}></div>
         </div>
-    )
+    );
 }
 
-export default LandingPageHeader
+export default LandingPageHeader;
