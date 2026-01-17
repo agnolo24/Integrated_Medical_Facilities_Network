34e182cbf43fcfa39d55ce3a8b6a975864199342
Amaljith <agnolo2024@gmail.com>
2026-01-17 13:15:59 +0530

    added header and footer to the RegisterAmbulance

---
+++ frontend/imfn/src/components/Hospital/ManageAmbulance/RegisterAmbulance/RegisterAmbulance.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HospitalHeader from '../../HospitalHeader/HospitalHeader'
+import AmbulanceForm from '../../../forms/AmbulanceForm/AmbulanceForm'
+import HospitalFooter from '../../HospitalFooter/HospitalFooter'
+
+function RegisterAmbulance() {
+  return (
+    <div>
+          <HospitalHeader />
+          <AmbulanceForm />
+          <HospitalFooter/>
+    </div>
+  )
+}
+
+export default RegisterAmbulance
