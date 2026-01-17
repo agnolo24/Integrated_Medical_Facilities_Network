c5653b4a1eba4aae5c7361f6bf122f3c7819ed2d
Anzil S <anzilshibu5@gmail.com>
2026-01-18 00:29:54 +0530

    function for getting doctor data to profile is added to views.py . serializer for edit doctor is added

--- backend/imfn/doctor/urls.py
+++ backend/imfn/doctor/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('getDoctorData/',views.getDoctorData,name="getDoctorData"),
     
 ]
--- backend/imfn/doctor/views.py
+++ backend/imfn/doctor/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, check_password
+
+from api.mongo import get_db
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import *
+
+from datetime import datetime
+
+from bson import ObjectId
 
 # Create your views here.
+
+@api_view(['GET'])
+def getDoctorData(request):
+    dr_id = request.query_params.get('dr_id')
+    print("id : ",dr_id)
+
+    db = get_db()
+
+    docter_coll = db['doctors']
+
+    qry = {
+        "login_id" : ObjectId(dr_id)
+    }
+    
+    doctor_cur = docter_coll.find_one(qry)
+    if not doctor_cur:
+        print("no data retreved")
+
+    else:
+        doctor_cur['_id'] = str(doctor_cur['_id'])
+        doctor_cur['login_id'] = str(doctor_cur['login_id'])
+        doctor_cur['hospital_login_id'] = str(doctor_cur['hospital_login_id'])
+        print(doctor_cur)
+
+
+    return Response(doctor_cur,status=status.HTTP_200_OK)
