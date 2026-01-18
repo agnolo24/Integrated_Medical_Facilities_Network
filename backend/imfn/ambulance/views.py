b2f732d4e21b35382d0a49bc35a6055e32ec1f41
Diane <dianemathew2002@gmail.com>
2026-01-18 12:29:08 +0530

    backend code of  get ambulance data is created

--- backend/imfn/ambulance/views.py
+++ backend/imfn/ambulance/views.py
@@ -19,4 +19,19 @@ from bson import ObjectId
 def getAmbulanceData(request):
     am_id=request.query_params.get('am_id')
     print(am_id)
-    return Response({},status=status.HTTP_200_OK)
+
+    db =get_db()
+    amb_col=db['ambulance']
+
+    amb=amb_col.find_one({"login_id":ObjectId(am_id)})
+    if not amb:
+        print("no data")
+    else:
+        amb['_id'] = str(amb['_id'])
+        amb['login_id'] = str(amb['login_id'])
+        amb['hospital_login_id'] = str(amb['hospital_login_id'])
+        print(amb)
+
+
+    
+    return Response(amb,status=status.HTTP_200_OK)
