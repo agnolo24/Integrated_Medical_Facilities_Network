3988e3cd8a5e50cc6b84d742a93083964e5f53b9
Anzil S <anzilshibu5@gmail.com>
2026-01-26 15:08:30 +0530

    now the doctor can add prescription (but can't load the data)

--- backend/imfn/doctor/urls.py
+++ backend/imfn/doctor/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('getDoctorData/',views.getDoctorData,name="getDoctorData"),
     path('get_patient_appointment/',views.get_patient_appointment,name= "get_patient_appointment"),
     path('get_patient_appointment_details/',views.get_patient_appointment_details,name= "get_patient_appointment_details"),
-    path('check_history_code/',views.check_history_code,name= "check_history_code")
+    path('check_history_code/',views.check_history_code,name= "check_history_code"),
+    path('add_prescription_by_doctor/',views.add_prescription_by_doctor,name= "add_prescription_by_doctor")
     
     
 ]
